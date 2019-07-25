3749c942b1909f894d15a85bdba3d91b7044a1ef
Christopher Arnold <36491899+carnold18@users.noreply.github.com>
2019-07-24 21:41:23 -0700

    added equal algo

---
+++ find_equal.rb
@@ -0,0 +1,29 @@
+# Create a function that takes three integer arguments (a, b, c) and returns the number of equal values.
+
+def equal(a, b, c)
+	
+end
+
+equal(3, 4, 3) 
+# => 2
+equal(1, 1, 1)
+# => 3
+equal(3, 4, 1) 
+# => 0
+
+# Your function must return 0, 2 or 3.
+
+
+# def equal(a, b, c)
+#	array=[a,b,c]
+#	new_array=array.uniq
+#	result=nil
+#	if new_array.length==1
+#		result=3
+#	elsif new_array.length==2
+#		result=2
+#	else
+#		result=0
+#	end
+#	result	
+# end
